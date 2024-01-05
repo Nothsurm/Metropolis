de8dbe0c048486486424f635a327c651dcd4a743
Mike Rushton <mrush94ton@gmail.com>
2024-01-05 17:43:02 +0000

    created side navbar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Navigation from './pages/Auth/Navigation';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className='py-3'>
+        <Outlet />
+      </main>
+    </>
   )
 }
 
