48245fa710233f2833524848a87c1e08ede77ef3
Mike Rushton <mrush94ton@gmail.com>
2024-01-06 17:41:38 +0000

    created profile ui and functionality

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -14,16 +14,87 @@ export default function Profile() {
 
     const {userInfo} = useSelector(state => state.auth)
 
-    const [updateProfile, {isLoading: loadingUpdateProfile}] = useProfileMutation;
+    const [updateProfile, {isLoading: loadingUpdateProfile}] = useProfileMutation();
 
     useEffect(() => {
         setUsername(userInfo.username)
         setEmail(userInfo.email)
     }, [userInfo.email, userInfo.username])
 
-    const dispatch = useDispatch()
-    
+    const dispatch = useDispatch();
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        if (password != confirmPassword) {
+            toast.error('Passwords do not match')
+        } else {
+            try {
+                const res = await updateProfile({_id: userInfo._id, username, email, password}).unwrap()
+                dispatch(setCredentials({...res}))
+                toast.success('Profile updated successfully')
+            } catch (error) {
+                toast.error(error?.data?.message || error.message)
+            }
+        }
+    }
+
   return (
-    <div>Profile</div>
+    <div className="container mx-auto p-4 mt-[10rem] text-white">
+        <div className="flex justify-center align-center md:flex md:space-x-4">
+            <div className="md:w-1/3">
+                <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+                <form onSubmit={submitHandler}>
+                    <div className="mb-4">
+                        <label className="block mb-2">Name</label>
+                        <input 
+                            type="text" 
+                            placeholder="Enter name..." 
+                            className="form-input p-4 rounded-sm w-full bg-zinc-600" 
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-2">Email</label>
+                        <input 
+                            type="email" 
+                            placeholder="Enter email..." 
+                            className="form-input p-4 rounded-sm w-full bg-zinc-600" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-2">Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Enter password..." 
+                            className="form-input p-4 rounded-sm w-full bg-zinc-600" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-2">Confirm Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Confirm password..." 
+                            className="form-input p-4 rounded-sm w-full bg-zinc-600" 
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-between">
+                        <button type='submit' className="bg-pink-500 py-2 px-4 rounded hover:bg-pink-600">Update</button>
+                        <Link to='/user-orders' className="bg-pink-600 py-2 px-4 rounded hover:bg-pink-700">
+                            My Orders
+                        </Link>
+                    </div>
+                </form>
+            </div>
+            {loadingUpdateProfile && <Loader />}
+        </div>
+    </div>
   )
 }
