8e26696e986e074e6041b3c14006020d011c3881
Mike Rushton <mrush94ton@gmail.com>
2024-01-06 17:22:06 +0000

    setup profile route and functionality

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -110,14 +110,20 @@ export default function Navigation() {
                             <li>
                                 <Link to='/admin/userlist' className="block px-4 py-2 hover:bg-zinc-800">Users</Link>
                             </li>
+                            <li>
+                                <Link to='/admin/profile' className="block px-4 py-2 hover:bg-zinc-800">Profile</Link>
+                            </li>
+                            <li>
+                                <Link to='/admin/logout' onClick={logoutHandler} className="block px-4 py-2 hover:bg-zinc-800">Logout</Link>
+                            </li>
                         </>
                     )}
 
                     <li>
-                        <Link to='/admin/profile' className="block px-4 py-2 hover:bg-zinc-800">Profile</Link>
+                        <Link to='/profile' className="block px-4 py-2 hover:bg-zinc-800">Profile</Link>
                     </li>
                     <li>
-                        <Link to='/admin/logout' onClick={logoutHandler} className="block px-4 py-2 hover:bg-zinc-800">Logout</Link>
+                        <Link to='/logout' onClick={logoutHandler} className="block px-4 py-2 hover:bg-zinc-800">Logout</Link>
                     </li>
                 </ul>
             )}
