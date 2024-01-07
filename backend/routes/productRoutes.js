c4483e3a20b375e8809ea193c3d0cc1ba5ba396a
Mike Rushton <mrush94ton@gmail.com>
2024-01-07 21:53:38 +0000

    created all products api and route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,12 @@ import express from 'express';
 import formidable from 'express-formidable';
 import { authenticate, authorizedAdmin } from '../middlewares/authMiddleware.js';
 import checkId from '../middlewares/checkId.js';
-import { addProduct, updateProductDetails, removeProduct } from '../controllers/productController.js';
+import { addProduct, updateProductDetails, removeProduct, fetchProducts } from '../controllers/productController.js';
 
 
 const router = express.Router()
 
-router.route('/').post(authenticate, authorizedAdmin, formidable(), addProduct);
+router.route('/').get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
 router.route('/:id').put(authenticate, authorizedAdmin, formidable(), updateProductDetails).delete(authenticate, authorizedAdmin, removeProduct)
 
 
