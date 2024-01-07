bbd55bab36ca3cab0d4fcc4e7f2532ad0466c5e8
Mike Rushton <mrush94ton@gmail.com>
2024-01-07 15:23:02 +0000

    create, read, edit and delete categories api functionality

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authenticate, authorizedAdmin } from '../middlewares/authMiddleware.js';
-import { createCategory } from '../controllers/categoryController.js';
+import { createCategory, updateCategory, removeCategory, listCategories, readCategory } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
 router.route('/').post(authenticate, authorizedAdmin, createCategory);
+router.route('/:categoryId').put(authenticate, authorizedAdmin, updateCategory).delete(authenticate, authorizedAdmin, removeCategory).get(readCategory)
+router.route('/categories').get(listCategories)
 
 export default router;
