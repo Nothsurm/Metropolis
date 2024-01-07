6acc7d09942c7423a03bdf96021964d658eabcc5
Mike Rushton <mrush94ton@gmail.com>
2024-01-07 17:12:40 +0000

    created manage category ui and functionality

--- frontend/src/components/CategoryForm.jsx
+++ frontend/src/components/CategoryForm.jsx
@@ -6,10 +6,10 @@ export default function CategoryForm({value, setValue, handleSubmit, buttonText 
         <form onSubmit={handleSubmit} className='space-y-3'>
             <input 
                 type="text" 
-                className='py-3 px-4 border rounded-lg w-full' 
+                className='py-3 px-4 border rounded-lg w-full bg-zinc-600' 
                 placeholder='Write category name...' 
                 value={value} 
-                onChange={e => setValue(e.target.value)}
+                onChange={(e) => setValue(e.target.value)}
             />
             <div className="flex justify-between">
                 <button className='bg-pink-500 text-white py-2 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50'>
