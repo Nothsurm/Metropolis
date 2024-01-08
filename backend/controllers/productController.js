7e7fa86a1a0c55826a0461333f6a53dbb7cd5a3c
Mike Rushton <mrush94ton@gmail.com>
2024-01-08 12:34:06 +0000

    created all products route and api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -85,6 +85,34 @@ const fetchProducts = asyncHandler(async (req, res) => {
         console.error(error)
         res.status(500).json(error.message)
     }
+});
+
+const fetchProductsById = asyncHandler(async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (product) {
+            return res.json(product)
+        } else {
+            res.status(404)
+            throw new Error('Product not found')
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({error: 'Product not found'})
+    }
+});
+
+const fetchAllProducts = asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find({})
+            .populate('category')
+            .limit(12)
+            .sort({createAt: -1})
+        res.json(products)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Server error'})
+    }
 })
 
-export { addProduct, updateProductDetails, removeProduct, fetchProducts };
+export { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductsById, fetchAllProducts };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,15 @@ import express from 'express';
 import formidable from 'express-formidable';
 import { authenticate, authorizedAdmin } from '../middlewares/authMiddleware.js';
 import checkId from '../middlewares/checkId.js';
-import { addProduct, updateProductDetails, removeProduct, fetchProducts } from '../controllers/productController.js';
+import { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductsById, fetchAllProducts } from '../controllers/productController.js';
 
 
 const router = express.Router()
 
 router.route('/').get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
-router.route('/:id').put(authenticate, authorizedAdmin, formidable(), updateProductDetails).delete(authenticate, authorizedAdmin, removeProduct)
+router.route('/allproducts').get(fetchAllProducts);
+router.route('/:id').put(authenticate, authorizedAdmin, formidable(), updateProductDetails).delete(authenticate, authorizedAdmin, removeProduct).get(fetchProductsById)
+
 
 
 
