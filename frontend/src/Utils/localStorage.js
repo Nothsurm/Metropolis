5c705455cb909b7d13d673ef055428d3d13035a5
Mike Rushton <mrush94ton@gmail.com>
2024-01-09 16:56:56 +0000

    created Local storage functionality for favourite products

---
+++ frontend/src/Utils/localStorage.js
@@ -0,0 +1,22 @@
+// Add a product to a local Storage
+export const addFavouriteToLocalStorage = (product) => {
+    const favourites = getFavouritesFromLocalStorage()
+    if (!favourites.some((p) => p._id === product._id)) {
+        favourites.push(product)
+        localStorage.setItem('favourites', JSON.stringify(favourites))
+    }
+};
+
+// Remove a product from a local Storage
+export const removeFavouriteFromLocalStorage = (productId) => {
+    const favourites = getFavouritesFromLocalStorage()
+    const updatefavourites = favourites.filter((product) => product._id != productId)
+
+    localStorage.setItem('favourites', JSON.stringify(updatefavourites))
+};
+
+// Retrieve favourites from a local Storage
+export const getFavouritesFromLocalStorage = () => {
+    const favouritesJSON = localStorage.getItem('favourites')
+    return favouritesJSON ? JSON.parse(favouritesJSON) : [];
+};
