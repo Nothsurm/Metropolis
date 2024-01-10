51251024d7b21413152fc674a97247759df4eae7
Mike Rushton <mrush94ton@gmail.com>
2024-01-10 21:55:42 +0000

    created order controller and post route

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { authenticate, authorizedAdmin } from '../middlewares/authMiddleware.js';
+import { createOrder } from '../controllers/orderController.js';
+
+const router = express.Router()
+
+router.route('/').post(authenticate, createOrder)
+
+export default router;
