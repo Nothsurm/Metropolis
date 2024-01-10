705b18251d3a1bf07b27c567c49c49b50c8e1f5c
Mike Rushton <mrush94ton@gmail.com>
2024-01-10 16:14:17 +0000

    added count to cart functionality

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -11,7 +11,8 @@ import './Navigation.css';
 import FavouritesCount from "../Products/FavouritesCount.jsx";
 
 export default function Navigation() {
-    const { userInfo } = useSelector(state => state.auth)
+    const { userInfo } = useSelector((state) => state.auth)
+    const { cartItems } = useSelector((state) => state.cart)
     const [dropdownOpen, setDropdownOpen] = useState(false)
     const [showSidebar, setShowSidebar] = useState(false)
 
@@ -69,6 +70,15 @@ export default function Navigation() {
             >
                 <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26}/>
                 <span className="hidden nav-item-name mt-[3rem]">CART</span>
+                <div className="absolute top-9 left-3">
+                    {cartItems.length> 0 && (
+                        <span>
+                            <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                                {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                            </span>
+                        </span>
+                    )}
+                </div>
             </Link>
             <Link 
                 to='/favourite' 
