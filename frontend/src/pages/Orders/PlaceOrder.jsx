56e04e1d67439636646f5755c650c61c53caff99
Mike Rushton <mrush94ton@gmail.com>
2024-01-13 22:01:31 +0000

    removed prices bug on place order summary ui

--- frontend/src/pages/Orders/PlaceOrder.jsx
+++ frontend/src/pages/Orders/PlaceOrder.jsx
@@ -84,22 +84,7 @@ export default function PlaceOrder() {
             <div className="mt-8">
                 <h2 className="text-2xl font-semibold mb-5">Order Summary</h2>
                 <div className="flex justify-between flex-wrap p-8 bg-[#181818]">
-                    <ul className="text-lg">
-                        <li>
-                            <span className="font-semibold mb-4">Items:</span> $ {cart.itemsPrice}
-                        </li>
-                        <li>
-                            <span className="font-semibold mb-4">Shipping:</span> $ {cart.shippingPrice}
-                        </li>
-                        <li>
-                            <span className="font-semibold mb-4">Tax:</span> $ {cart.taxPrice}
-                        </li>
-                        <li>
-                            <span className="font-semibold mb-4">Total:</span> $ {cart.totalPrice}
-                        </li>
-                    </ul>
                     {error && <Message variant={'danger'}>{error.data.message}</Message>}
-
                     <div>
                         <h2 className="text-2xl font-semibold mb-4">Shipping</h2>
                         <p>
