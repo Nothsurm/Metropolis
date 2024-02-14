c08f238eb599f28bbe0af73eadde8fe9a9dcefe1
Mike Rushton <mrush94ton@gmail.com>
2024-02-14 20:44:59 +0000

    installed nodepolyfills

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    NgmiPolyfill()
+    NgmiPolyfill(),
+    nodePolyfills()
   ],
   server: {
     proxy: {
